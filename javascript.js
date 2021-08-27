e8ff65f1044b50b3dd7428cc2b35aa7ff20be652
Victor Lourenço <victorhlo12@hotmail.com>
2021-08-27 16:49:46 -0300

    tomeagua_v1.4

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(){
+function toggleMenu() {
     const nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
