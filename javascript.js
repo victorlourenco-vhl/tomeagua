e49fafaca0e31a1dcd41734a0039629b40d2b092
Victor Lourenço <victorhlo12@hotmail.com>
2021-09-06 19:42:21 -0300

    calculo-copos

--- javascript.js
+++ javascript.js
@@ -1,18 +1,27 @@
-function img01(){
-    document.getElementById("copo1").src="imagens/copo-vazio.png";
-}
 function calcular(){
     var litros = document.getElementById("litros").value;
     console.log(litros);
-    if(litros == 1){
-        //document.querySelectorAll("#copo1, #copo2, #copo3, #copo4").src="imagens/copo-cheio.png";
-        //document.querySelectorAll("copo1", "copo2", "copo3", "copo4").src="imagens/copo-cheio.png";
-        var x = document.querySelectorAll("#copo1, #copo2, #copo3, #copo4");
-        var i;
-        for (i = 0; i < x.length; i++) {
-            x[i].src="imagens/copo-cheio.png";
-          
-        } 
-   
+    var qtncopos = litros / 0.250;
+    let copoids = [];
+    var str = "#copo";
+    for(var qtn = 1; qtn <= qtncopos; qtn++){
+        str  += qtn;
+        copoids.push(str);
+        str = "#copo";
+    }
+
+    console.log(copoids);
+
+    var y = document.getElementsByClassName("copo-vazio");
+    for (var n = 0; n < y.length; n++) {
+        y[n].src='imagens/copo-vazio.png';
+    }
+    var x = document.querySelectorAll(copoids);
+    for (var i = 0; i < x.length; i++) {
+        x[i].src='imagens/copo-cheio.png';
     }
 }
+function img01(){
+    document.getElementById("copo1").src="imagens/copo-vazio.png";
+}
+//document.querySelectorAll("#copo1, #copo2, #copo3, #copo4").src="imagens/copo-cheio.png";
