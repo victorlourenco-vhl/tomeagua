f1c54d43075d8726e434db594d007964078b51dd
Victor Lourenço <victorhlo12@hotmail.com>
2021-09-08 21:59:29 -0300

    troca-imagem

--- javascript.js
+++ javascript.js
@@ -1,16 +1,14 @@
 function calcular(){
+   
     var litros = document.getElementById("litros").value;
-    console.log(litros);
     var qtncopos = litros / 0.250;
-    let copoids = [];
+    var copoids = [];
     var str = "#copo";
     for(var qtn = 1; qtn <= qtncopos; qtn++){
         str  += qtn;
         copoids.push(str);
         str = "#copo";
     }
-    console.log(copoids);
-
     var y = document.getElementsByClassName("copo-vazio");
     for (var n = 0; n < y.length; n++) {
         y[n].src='imagens/copo-vazio.png';
@@ -20,44 +18,48 @@ function calcular(){
         x[i].src='imagens/copo-cheio.png';
     }
     // IDs Botao
+    var btnids = [];
     var strBtn = "#btn-copo";
-    let btnids = [];
     for(var btn = 1; btn <= qtncopos; btn ++){
         strBtn += btn;
         btnids.push(strBtn);
         strBtn = "#btn-copo";
     }    
-    console.log(btnids);
-    var getIDs = document.querySelectorAll(btnids);
-    var click = 0;
-    for(let c = 0; c < getIDs.length; c++){
-       
-        getIDs[c].addEventListener("click", function(e){ 
-            click += 1;
-            var v = this.value;
-           
-            var copoID = "copo" + v;
-            document.getElementById(copoID).src="imagens/copo-vazio.png";
+
+    var contador = 10;
+    function teste(){
+        
+        var estadoCopo = "";
+        var v = this.value;
+        var copoID = "copo" + v;  
+        var z = document.getElementById(copoID).src;
+        var ultima = z.substring(z.lastIndexOf("-")+1);
+        console.log("Ultima " + ultima);
+
+        
+        if (ultima === "cheio.png" ){
+            estadoCopo =  "";
+            estadoCopo = "imagens/copo-vazio.png";
             
-            var z = document.getElementById(copoID).src;
-            var ultima = z.substring(z.lastIndexOf("-")+1);
+        }
+        if (ultima === "vazio.png" ){
+            estadoCopo =  "";
+            estadoCopo = "imagens/copo-cheio.png";
            
+        }
          
-            
-            console.log("clicks" + click);
-            console.log(ultima);
-          
-            if (click == 2 && ultima == "vazio.png" ){
-                click = 1;
-                document.getElementById(copoID).src="imagens/copo-cheio.png";
-               
-            }else if (click == 2 && ultima == "cheio.png" ){
-                click = 1;
-                document.getElementById(copoID).src="imagens/copo-vazio.png";
-                
-            }
-        })
+        document.getElementById(copoID).src=estadoCopo ;
+        console.log("contador" + contador)
+        
     }
+    var getIDs = document.querySelectorAll(btnids);
+    for(var c = 0; c < getIDs.length; c++){
+        getIDs[c].addEventListener("click", teste)
+        
+    }   
+    
+
+    
 }
 /*
 http://127.0.0.1:5501/imagens/copo-vazio.png
